aa22bdf4d4f683844da3fa222ac2328683397540
Rabi2206d <119856644+Rabi2206d@users.noreply.github.com>
2025-03-25 12:27:05 +0500

    someChanges

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+
+const fetchUser = (req,res, next)=>
+{
+    const token = req.header('auth-token');
+    if(!token)
+    {
+        return res.status(401).json({err : 'Please using a valid token number'})
+    }
+    try {
+        
+        const decode = jwt.verify(token , 'HMS')
+        req.userid = decode.userid
+        next()
+    } catch (error) {
+        return res.status(500).json({err : 'Internal Server error'})
+        
+    }
+}
+
+
+export default fetchUser
